221bed267445aa4de90dd506c30103d470a239ee
Camilo Vargas <ca06vargas@gmail.com>
2026-01-28 21:11:39 -0500

    Add Prettier and ESLint configuration, update package dependencies, and format code
    - Introduced .prettierignore to exclude specific files from formatting.
    - Updated ESLint configuration for consistent code style.
    - Added Prettier configuration file for code formatting rules.
    - Enhanced package.json with new scripts for formatting and linting.
    - Updated pnpm-lock.yaml with new dependencies including husky and lint-staged.
    - Reformatted various files for consistent styling, including layout and globals.css.
    - Added Husky pre-commit hook to run lint-staged for pre-commit checks.
    - Configured VSCode settings for automatic formatting on save.
    - Adjusted TypeScript configuration for improved type checking.
    - Ensured consistent use of single quotes across the codebase.
    - Minor adjustments to CSS for better readability and maintainability.

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 120,
+    singleQuote: true,
+    semi: true,
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+};
+
+export default config;
